ad2fbbf92f9546e6c07794e0ca479ad16a83acc8
bluejet80 <know_jarett@yahoo.com>
2023-07-16 16:21:53 -0500

    starting dev process

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div className="section"><h2>Left Section</h2>
+              <p>This is the left Section</p>
+          </div>
+          <div className="section"><h2>Right Section</h2><p>This is the right section.</p></div>
       </header>
+      <body className="main">
+        <div><p>This is a paragraph</p></div>
+      </body>
     </div>
   );
 }
